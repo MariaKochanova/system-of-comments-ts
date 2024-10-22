d83a84f623634bc9a8e8d42215ae80b11fa4f040
Mariya Kochanova <balalum@gmail.com>
2024-10-22 22:43:36 +0400

    переделала структуру модулей

--- src/comment.ts
+++ src/comment.ts
@@ -1,17 +1,3 @@
 export default class CommentClass {
-    id: number;
-    text: string;
-    date: Date;
-    rating: number;
-    autor: string;
-    photoUrl: string;
-
-    constructor(id: number, text: string, date: Date, rating: number, autor: string, photoUrl: string) {
-        this.id = id;
-        this.text = text;
-        this.date = date;
-        this.rating = rating;
-        this.autor = autor;
-        this.photoUrl = photoUrl
-    }
+    
 }
