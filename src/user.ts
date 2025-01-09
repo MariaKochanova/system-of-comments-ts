ad8cc5fabf8ab927442843e34dd43dadccfe0258
Mariya Kochanova <balalum@gmail.com>
2025-01-09 14:01:13 +0400

    продолжение

--- src/user.ts
+++ src/user.ts
@@ -1,12 +1,38 @@
+// export default class User {
+//     fullName: string;
+//     photoUrl: string;
+
+//     constructor(fullName: string, photoUrl: string) {
+//         this.fullName = fullName;
+//         this.photoUrl = photoUrl;
+
+//         // this.createUserFromApi();
+//     }
+
+//     static async createUserFromApi(): Promise<User> {
+//         const response = await fetch('https://randomuser.me/api/');
+//         const person = await response.json();
+
+//         const firstName = person.results[0].name.first;
+//         const lastName = person.results[0].name.last;
+//         const fullName = `${firstName} ${lastName}`;
+
+//         const photoUrl = person.results[0].picture.medium;
+
+//         return new User(fullName, photoUrl);
+//     }
+// }
+
 export default class User {
     fullName: string;
     photoUrl: string;
 
-    constructor(fullName: string, photoUrl: string) {
+    private constructor(fullName: string, photoUrl: string) {
         this.fullName = fullName;
         this.photoUrl = photoUrl;
     }
 
+    // Фабричный метод для создания пользователя
     static async createUserFromApi(): Promise<User> {
         const response = await fetch('https://randomuser.me/api/');
         const person = await response.json();
@@ -14,9 +40,15 @@ export default class User {
         const firstName = person.results[0].name.first;
         const lastName = person.results[0].name.last;
         const fullName = `${firstName} ${lastName}`;
-
         const photoUrl = person.results[0].picture.medium;
 
+        // Создаём экземпляр User с полученными данными
         return new User(fullName, photoUrl);
     }
 }
+
+// Пример использования фабричного метода
+// (async () => {
+//     const user = await User.createUserFromApi();
+//     console.log(user);
+// })();
